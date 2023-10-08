45bd5475e9a1f837ead3c6029fd8951fb921d5f8
HiDolen <820859278@qq.com>
2023-10-08 21:31:36 +0800

    修改插件信息初始化相关内容

--- basic_plugins/init_when_startup/init_plugin_info.py
+++ basic_plugins/init_when_startup/init_plugin_info.py
@@ -36,40 +36,4 @@ async def _():
                 logger.warning(f"插件 {name} 未正确设置 __plugin_meta__")
         else:
             continue
-
-    # TODO 删除已经不存在的插件信息。也就是垃圾清理环节
         
-
-    # for matcher in all_matchers:
-    #     matcher: Matcher = matcher
-    #     try:
-    #         module = matcher.plugin.module
-    #     except AttributeError:
-    #         continue
-
-    #     try:
-    #         plugin_info = module.__getattribute__("__plugin_info__")
-    #         plugin = matcher.plugin_name
-    #         try:
-    #             info = {
-    #                 "plugin_name": plugin_info["plugin_name"],
-    #                 "description": plugin_info["description"],
-    #                 "usage": plugin_info["usage"],
-    #                 "default_status": plugin_info["default_status"]
-    #             }
-    #             if plugin_manager.plugin_info.info_is_different(plugin, info):
-    #                 plugin_manager.plugin_info.set_plugin_info(plugin, **info)
-    #                 logger.info(f"插件 `{plugin_info['plugin_name']}({plugin})` 信息更新")
-    #         except AttributeError:
-    #             logger.warning(f"插件 {plugin} 未正确设置 __plugin_info__")
-    #     except AttributeError:
-    #         pass
-
-    all_plugins = [
-        matcher.plugin.name for matcher in all_matchers if matcher.plugin]
-    all_plugins_in_manager = plugin_manager.plugin_info.get_all_plugin()
-    # 删除已经不存在的插件
-    plugins_to_delete = set(all_plugins_in_manager) - set(all_plugins)
-    for plugin in plugins_to_delete:
-        plugin_manager.plugin_info.remove_plugin(plugin)
-        logger.success(f"插件 {plugin} 信息已删除")
